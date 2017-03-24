1b810531c91e3679f6823c09e5074065869e3ae2
Tyler Ches <ttches@gmail.com>
2017-03-23 21:35:45 -0700

    Finished Nav Styles

---
+++ src/containers/Nav.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <div className="nav-container">
+            <img src="http://vignette4.wikia.nocookie.net/logopedia/images/2/26/Twitch_logo.svg/revision/latest/scale-to-width-down/639?cb=20140727180649" width="100px" alt=''/>
+            <form onSubmit='addStreamer(document.getElementById("addStreamInput").value.toLowerCase());return false;'>
+              <div style={{position: 'relative'}}>
+                <div id="addStreamIcon"><p><i className="fa fa-user-o" aria-hidden="true"></i></p></div>
+                <input id="addStreamInput" type="text" placeholder="Add a streamer" />
+              </div>
+            </form>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
